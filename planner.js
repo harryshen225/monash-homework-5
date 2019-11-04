1551f5e530761aa5f9c1e84fcd4023d909242d93
Harry Shen <harshen@cisco.com>
2019-11-04 15:31:28 +1100

    push

--- planner.js
+++ planner.js
@@ -1,38 +1,81 @@
 // (Done)render the date and time in the header   moment.js library
-// color code red = current, green= future, grey =past
+// (Done)color code red = current, green= future, grey =past
 // able to save to the local storage
-// clean up the css
+// (Done)clean up the css
 // add calendar selector
-
+let daySchedule = {};
 
 $("#currentDay").html(currentDate());
 renderDayPlanner();
+renderSchedule();
 
-function currentDate(){
-    return moment().format('dddd, MMMM Do, YYYY'); 
-
-}
-
-function rednerColorCoding(){
-
-}
-
-function renderDayPlanner(){
-    for(var i = 9;i<12;i++){
-        rednerInputGroup(i,"a.m");
+function renderSchedule() {
+    let storedSchedule = JSON.parse(localStorage.getItem("today"));
+    console.log(localStorage.getItem("today"));
+    if (storedSchedule) {
+        daySchedule = storedSchedule;
+        renderHourSchedule();
     }
-    rednerInputGroup(12,"p.m");
+    else {
+        for (let i = 5; i < 18; i++) {
+            let strI = i.toString();
+            daySchedule[i] = "";
+        }
+        renderHourSchedule();
+    }
+}
 
-    for(var i = 1;i<6;i++){
-        rednerInputGroup(i,"p.m");
+$(".container").on("click", "button", function () {
+    let inputID = $($(this).parents()[1]).attr('id');
+    console.log($(`#${inputID} textarea`)[0]);
+    if ($(`#${inputID} textarea`)[0].value) {
+        daySchedule[inputID] = $(`#${inputID} textarea`)[0].value;
+        localStorage.setItem("today", JSON.stringify(daySchedule));
+    }
+
+})
+
+function renderHourSchedule() {
+    for (let [key, value] of Object.entries(daySchedule)) {
+        $($(`#${key} textarea`)[0]).val(value);
+    }
+}
+
+
+function currentDate() {
+    return moment().format('dddd, MMMM Do, YYYY');
+}
+
+function getCurrentHour() {
+    return parseInt(moment().format('HH'));
+}
+
+
+function renderDayPlanner() {
+    for (var i = 9; i < 12; i++) {
+        rednerInputGroup(i, "a.m", i);
+    }
+    rednerInputGroup(12, "p.m", 12);
+
+    for (var i = 1; i < 6; i++) {
+        rednerInputGroup(i, "p.m", 12 + i);
     }
 
 }
 
-function rednerInputGroup(hour,amPM){
-    var inputPrepend = $("<div>").addClass("input-group-prepend hour-entry").append($("<span>").addClass("hour col-sm-12").text(hour +" "+ amPM));
-    var inputField = $("<textarea>").addClass("form-control").attr("aria-label","Task details").attr("field-id",`${hour}${amPM}`);
+function rednerInputGroup(hour, amPM, militaryHour) {
+    var inputPrepend = $("<div>").addClass("input-group-prepend hour-entry").append($("<span>").addClass("hour col-sm-12").text(hour + " " + amPM));
+    var inputField = $("<textarea>").addClass("form-control").attr("aria-label", "Task details").attr("field-id", `${hour}${amPM}`);
+    if (getCurrentHour() === militaryHour) {
+        inputField.addClass("present");
+    }
+    else if (getCurrentHour() <= militaryHour) {
+        inputField.addClass("future");
+    }
+    else {
+        inputField.addClass("past");
+    }
     var inputAppend = $("<div>").addClass("input-group-apppend hour-entry").append($("<button>").addClass("btn btn-outline-secondary col-sm-12 saveBtn").html('<i class="far col-sm-12 fa-save"></i>'));
-    var newInputGroup = $("<div>").addClass("input-group").append(inputPrepend,inputField,inputAppend);
+    var newInputGroup = $("<div>").addClass("input-group").attr("id", `${militaryHour}`).append(inputPrepend, inputField, inputAppend);
     $(".container").append(newInputGroup);
 }
